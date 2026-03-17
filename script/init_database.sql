37703c0f8e117cb767456c1a4dfe0b4b80020b45
AJ MURULANE <apfeswahojacky@gmail.com>
2026-03-17 13:40:09 +0200

    Create Data Warehouse database with schemas
    This script creates a Data Warehouse database with three schemas: Bronze, Silver, and Gold. Ensure necessary permissions and review database and schema names before execution.

--- script/init_database.sql
+++ script/init_database.sql
@@ -1,1 +1,45 @@
+ /*
+This script creates a Data Warehouse database with three schemas: Bronze, Silver, and Gold.
+
+WARNING: Before running this script, ensure that you have the necessary permissions to create databases
+and schemas on the SQL Server instance. Additionally, make sure to review and modify the database 
+name and schema names as needed to fit your specific requirements.
+*/
+
+
+
+USE master;
+Go 
+
+-- Drop and recrete the DataWareHouse database 
+IF EXISTS (SELECT name FROM sys.databases WHERE name = 'DataWareHouse')	
+BEGIN
+	ALTER DATABASE DataWareHouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWareHouse;
+END;
+Go
+
+-- Create the 'DataWareHouse' database
+CREATE DATABASE DataWareHouse;
+GO
+
+
+USE DataWareHouse;
+GO
+
+--Create the Bronze, Silver, and Gold schemas
+
+
+CREATE SCHEMA bronze;
+GO
+
+
+CREATE SCHEMA Silver;
+GO
+
+
+CREATE SCHEMA Gold;
+GO
+
+
 
